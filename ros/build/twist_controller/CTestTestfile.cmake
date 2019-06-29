f63e16b31520e1166444f7f115dbaeb5ec4e24c3
sepr88 <sebastian.prandl@gmail.com>
2019-06-29 18:34:13 +0200

    removed the now ignored directories ros/build and ros/devel

--- ros/build/twist_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/basti/Udacity/CarND-Capstone/ros/src/twist_controller
-# Build directory: /home/basti/Udacity/CarND-Capstone/ros/build/twist_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
